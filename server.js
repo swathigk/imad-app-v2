0949a611f49785f32fd2321566bbfb3dd072a7a3
swathigk <swathikamath69@gmail.com>
2017-02-25 16:03:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,11 +16,11 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-    res.send('Article two requestes and will be sevred here')
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
 app.get('/article-three',function(req,res){
-    res.send('Article three requestes and will be sevred here')
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
