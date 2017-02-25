7c7f118b589c6a8d4dd45c6206972470fc8f00a6
swathigk <swathikamath69@gmail.com>
2017-02-25 23:05:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,7 +29,7 @@ submit.onclick=function(){
     //make request to server
     
     //capture list
-    var name=['name2','name2','name3'];
+    var names=['name2','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++)
     list='<li'+names[i]+'</li>';
