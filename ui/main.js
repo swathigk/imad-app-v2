b7852f40980e99678e203a3cf19033047be95171
swathigk <swathikamath69@gmail.com>
2017-02-25 20:57:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,22 @@
 var button=document.getElementById('counter');
-var counter=0;
+
 button.onclick=function(){
 //make a request to counter endpoint
+var request=new XMLHttpRequest();
 
 
 //capture response and store
-
+request.onreadystatechange=function(){
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var counter=request.responseText;
+            var span=document.getElementById('count');
+            span.innerHTML=counter.toString();
+        }
+    }
+};
 
 //render the variable in the correct span
-counter=counter+1;
-var span=document.getElementById('count');
-span.innerHTML=counter.toString();
-    
+request.open('GET','http://swathigk.imad.hasura-app.io/counter',true);
+request.send(null);
 };
