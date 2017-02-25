824271e55c5d45b81da24bb983af581d6e38a072
swathigk <swathikamath69@gmail.com>
2017-02-25 23:03:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,19 @@ request.onreadystatechange=function(){
 request.open('GET','http://swathigk.imad.hasura-app.io/counter',true);
 request.send(null);
 };
+
+
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    //make request to server
+    
+    //capture list
+    var name=['name2','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++)
+    list='<li'+names[i]+'</li>';
+}
+var ul=document.getElementById('namelist');
+ul.innerHTML=list;
