88f3e9b9e7180975947d63722c18e494b5a34481
swathigk <swathikamath69@gmail.com>
2017-02-25 19:39:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var element=document.getElementById('main-text');
 element.innerHTML='new value';
 //move the image
 
-var img=document.getElementById('img');
+var img=document.getElementById('madi');
 img.onClick=function(){
     img.style.marginLeft='100px';
 };
