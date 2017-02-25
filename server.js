46755fb03f8e47fd5d4bfbb2bc445108c54299b7
swathigk <swathikamath69@gmail.com>
2017-02-25 18:04:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,42 +27,42 @@ var articleone= {
 };
 
 function createtemplate(data){
-var title=data.title;
-var date=data.date;
-var heading=data.heading;
-var content=data.content;
-
-var htmltemplate=`<html>
-  <head>
-      <title>
-          ${title} 
-      </title>
-      <meta name="viewport" content="width-device-width,initial-scale-1"/>
-      <link href="/ui/style.css" rel="stylesheet" />
-     
-  </head>  
-    <body>
-        <div class="container">
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+    var htmltemplate=`<html>
+      <head>
+          <title>
+              ${title} 
+          </title>
+          <meta name="viewport" content="width-device-width,initial-scale-1"/>
+          <link href="/ui/style.css" rel="stylesheet" />
+         
+      </head>  
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">HOME</a>
+                </div>
+            </hr>
+            <h3>
+                ${heading}
+            </h3>
             <div>
-                <a href="/">HOME</a>
+                ${date}
             </div>
-        </hr>
-        <h3>
-            ${heading}
-        </h3>
-        <div>
-            ${date}
-        </div>
-        <div>
-           
-           ${content}
-    
-        </div>
-        </div>
-    </body>
-    
-    
-</html>
+            <div>
+               
+               ${content}
+        
+            </div>
+            </div>
+        </body>
+        
+        
+    </html>
 `;
 return htmltemplate;
 }
@@ -76,7 +76,7 @@ app.get('/', function (req, res) {
 
 //when this url is reuqested this fn will execute
 app.get('/article-one',function(req,res){
-    res.send(createtemplate(article-one));
+    res.send(createtemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
