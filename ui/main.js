1975dcd9fd3ec6f3c6ab95eb4019da887db21a35
swathigk <swathikamath69@gmail.com>
2017-03-13 17:06:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,6 @@ submit.onclick=function(){
   var password=document.getElementById('password').value;
   console.log(username);
   console.log(password);
-  
   request.open('POST','http://swathigk.imad.hasura-app.io/login',true);
   request.setRequestHeader('Content-Type','application/json');
   request.send(JSON.stringify({username:username,password:password}));
