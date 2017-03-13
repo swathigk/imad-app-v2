6fc4b23ee44e3469045838197258a0006e30fe6e
swathigk <swathikamath69@gmail.com>
2017-03-13 18:45:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,6 +144,11 @@ app.get('/check-login',function(req,res){
 
 });
 
+app.get('/logout',function(req,res){
+    delete req.session,auth;
+    res.send('logged out');
+});
+
 var pool=new Pool(config);
 app.get('/test-db',function(req,res)
 {
