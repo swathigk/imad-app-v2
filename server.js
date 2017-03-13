e6b10edada1201a21f2bde47b8745cfcadbfbdb7
swathigk <swathikamath69@gmail.com>
2017-03-13 11:13:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ return htmltemplate;
 app.get('/', function (req, res) {            
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var Pool=new Pool(config);
+var pool=new Pool(config);
 app.get('/test-db',function(req,res)
 {
 
